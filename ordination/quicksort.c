ddc32eef063a28953413ac47cbf2ddfe20abb1c7
Anderson Fernandes <fernandesanderson14@gmail.com>
2016-07-20 22:59:54 -0300

    Improve quicksort.c
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- ordination/quicksort.c
+++ ordination/quicksort.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+#define MAX 10
+
+void swap(int *a, int *b);
+void quicksort(int *v, int size);
+void print_array(int *v, int size);
+
+int main() {
+  int v[MAX] = { 3, 6, 8, 10, 45, 2, 1, 90, 6, 12 };
+  int i;
+
+  printf("Before: "); print_array(v, MAX);
+
+  quicksort(v, MAX);
+
+  printf("After:  "); print_array(v, MAX);
+
+  return 0;
+}
+
+
+void quicksort(int *v, int size) {
+  if(size <= 1) return;
+
+  int pivot = v[size / 2];
+  int left = 0, right = size - 1;
+
+  while(left <= right) {
+    while(v[left] <= pivot) ++left;
+    while(v[right] > pivot) --right;
+
+    if(left < right) swap(&v[left], &v[right]);
+  }
+
+  quicksort(v, right);
+  quicksort(v+left, size-left);
+}
 
 void swap(int *a, int *b) {
   int aux = *a;
@@ -6,33 +42,10 @@ void swap(int *a, int *b) {
   *b = aux;
 }
 
-void quicksort(int *v, int size) {
-  if(size <= 1) {
-    return;
-  } else {
-    int pivot = v[size/2];
-    int left = 0, right = size-1;
-
-    while(left < right) {
-      while(v[left] < pivot) ++left;
-      while(v[right] > pivot) --right;
-
-      if(left < right) swap(&v[left], &v[right]);
-    }
-
-    quicksort(v, right);
-    quicksort(v + left, size - left);
-  }
-}
-
-int main() {
-  int v[10] = { 3, 6, 8, 10, 45, 2, 1, 90, 4, 12 };
+void print_array(int *v, int size) {
   int i;
-
-  quicksort(v, 10);
-
-  for(i = 0; i < 10; ++i) {
-    printf(">> %d\n", v[i]);
+  for(i = 0; i < size; ++i) {
+    printf("%d ", v[i]);
   }
-
+  printf("\n");
 }
